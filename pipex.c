f09da020e53a4ffd24f2e0ec5a4e9e78a0fc1a46
Nazar <nizarbonex1@gmail.com>
2022-10-11 19:44:16 +0200

    hfh

--- pipex.c
+++ pipex.c
@@ -15,8 +15,20 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "libft.h"
 
-int	pipex(int argc, char **argv, char **envp)
+char	**parsing_arguments(char **envp)
+{
+	char	*envp_path;
+	char	**my_path;
+	char	**args;
+
+	envp_path = ft_strdup(envp);
+	my_path = ft_split(envp, ":");
+	args = ft_split(argv[2], " ");
+}
+
+int	main(int argc, char **argv, char **envp)
 {
 	int	fd[2];
 	int	pid1;
@@ -27,7 +39,7 @@ int	pipex(int argc, char **argv, char **envp)
 	if (argc > 5 || argc < 5)
 		return (NULL);
 	infd = open(argv[1], O_RDONLY);
-	oufd = open(argv[4], O_RDWR, O_CREAT);
+	oufd = open(argv[4], O_RDWR, O_CREAT, 0777);
 	if (pipe(fd) == -1)
 		perror("Unable to pipe the situation!");
 	pid1 = fork();
@@ -35,6 +47,9 @@ int	pipex(int argc, char **argv, char **envp)
 		perror("Fork faild");
 	if (pid1 == 0)
 	{
+		int	i;
+		char	*cmd;
+
 		if (dup2(infd, 0) < 0)
 			return (0);
 		if (dup2(fd[1], 1) < 0)
@@ -43,18 +58,31 @@ int	pipex(int argc, char **argv, char **envp)
 		// close(fd[1]);
 		close(infd);
 		// close(oufd);
-		execve();
+		i = -1;
+		while (mypaths[++i])
+		{
+    		cmd = ft_join(mypaths[i], ag[2]);
+    		execve(cmd, mycmdargs, envp);
+    		free(cmd);
+		}
 		exit(/*EXIT_FAILURE*/0);
 	}
+	pid2 = fork();
+	if (pid2 < 0)
+		perror("second child faild!");
+	if (pid2 == 0)
+	{
+		if (dup2(oufd, 1) < 0)
+			return (0);
+		if (dup2(fd[0], 0) < 0)
+			return (0);
+			// close(fd[0]);
+			close(fd[1]);
+			// close(infd);
+			close(oufd);
+			execve();
+			exit(/*EXIT_FAILURE*/0);
+	}
 	waitpid(pid1, NULL, 0);
-	if (dup2(oufd, 1) < 0)
-		return (0);
-	if (dup2(fd[0], 0) < 0)
-		return (0);
-		// close(fd[0]);
-		close(fd[1]);
-		// close(infd);
-		close(oufd);
-		execve();
-		exit(/*EXIT_FAILURE*/0);
+	waitpid(pid2, NULL, 0);
 }
